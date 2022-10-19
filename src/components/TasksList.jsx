54024bcde809ccffa6ccd2eb38166ea1a5c8924c
Jorge <aportuvas@outlook.es>
2022-10-19 21:45:05 +0200

    add: supabase en vercel

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -2,30 +2,33 @@ import { useTasks } from "../context/TasksContext";
 import { useEffect } from "react";
 import TaskCard from "./TaskCard";
 
-function TasksList({done=false}) {
-  const { tasks, getTasks, loading } = useTasks();
+function TasksList({ done = false }) {
+   const { tasks, getTasks, loading } = useTasks();
 
-  useEffect(() => {
-    getTasks(done);
-  }, [done]);
+   useEffect(() => {
+      getTasks(done);
+   }, [done]);
 
-  function renderTasks() {
-    if (loading) {
-      return <p>Loading...</p>;
-    } else if (tasks.length === 0) {
-      return <p>No tasks found</p>;
-    } else {
-      return (
-        <ul className="tasks-list">
-          {tasks.map((task) => (
-            <TaskCard task={task} key={task.id} />
-          ))}
-        </ul>
-      );
-    }
-  }
+   function renderTasks() {
+      if (loading) {
+         return <p>Loading...</p>;
+      } else if (tasks.length === 0) {
+         return <p>No tasks found</p>;
+      } else {
+         return (
+            <ul className="tasks-list">
+               {tasks.map((task) => (
+                  <TaskCard
+                     task={task}
+                     key={task.id}
+                  />
+               ))}
+            </ul>
+         );
+      }
+   }
 
-  return <div>{renderTasks()}</div>;
+   return <div>{renderTasks()}</div>;
 }
 
 export default TasksList;
--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -4,97 +4,96 @@ import { supabase } from "../supabase/client";
 export const TasksContext = createContext();
 
 export const useTasks = () => {
-  const context = useContext(TasksContext);
-  if (!context)
-    throw new Error(
-      "useTasks must be used within a TasksContextProvider"
-    );
-  return context;
+   const context = useContext(TasksContext);
+   if (!context)
+      throw new Error(
+         "useTasks must be used within a TasksContextProvider"
+      );
+   return context;
 };
 
 export const TasksContextProvider = ({ children }) => {
-  const [tasks, setTasks] = useState([]);
-  const [adding, setAdding] = useState(false);
-  const [loading, setLoading] = useState(false);
+   const [tasks, setTasks] = useState([]);
+   const [adding, setAdding] = useState(false);
+   const [loading, setLoading] = useState(false);
 
-  const getTasks = async (done = false) => {
-    setLoading(true);
-    const user = supabase.auth.user();
-    const { error, data } = await supabase
-      .from("tasks")
-      .select()
-      .eq("userId", user.id)
-      .eq("done", done)
-      .order("id", { ascending: true });
-
-    if (error) throw error;
-
-    setTasks(data);
-    setLoading(false);
-  };
-
-  const createTask = async (taskName) => {
-    setAdding(true);
-    try {
+   const getTasks = async (done = false) => {
+      setLoading(true);
       const user = supabase.auth.user();
       const { error, data } = await supabase
-        .from("tasks")
-        .insert({
-          userId: user.id,
-          name: taskName,
-        });
+         .from("tasks")
+         .select()
+         .eq("userId", user.id)
+         .eq("done", done)
+         .order("id", { ascending: true });
 
       if (error) throw error;
 
-      setTasks([...tasks, ...data]);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setAdding(false);
-    }
-  };
+      setTasks(data);
+      setLoading(false);
+   };
 
-  const deleteTask = async (id) => {
-    const user = supabase.auth.user();
+   const createTask = async (taskName) => {
+      setAdding(true);
+      try {
+         const user = supabase.auth.user();
+         const { error, data } = await supabase
+            .from("tasks")
+            .insert({
+               userId: user.id,
+               name: taskName,
+            });
 
-    const { error, data } = await supabase
-      .from("tasks")
-      .delete()
-      .eq("userId", user.id)
-      .eq("id", id);
+         if (error) throw error;
 
-    if (error) throw error;
+         setTasks([...tasks, ...data]);
+      } catch (error) {
+         console.error(error);
+      } finally {
+         setAdding(false);
+      }
+   };
 
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
+   const deleteTask = async (id) => {
+      const user = supabase.auth.user();
 
-  const updateTask = async (id, updateFields) => {
-    const user = supabase.auth.user();
+      const { error } = await supabase
+         .from("tasks")
+         .delete()
+         .eq("userId", user.id)
+         .eq("id", id);
 
-    const { error, data } = await supabase
-      .from("tasks")
-      .update(updateFields)
-      .eq("userId", user.id)
-      .eq("id", id);
+      if (error) throw error;
 
-    if (error) throw error;
+      setTasks(tasks.filter((task) => task.id !== id));
+   };
 
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
+   const updateTask = async (id, updateFields) => {
+      const user = supabase.auth.user();
 
-  return (
-    <TasksContext.Provider
-      value={{
-        tasks,
-        getTasks,
-        createTask,
-        adding,
-        loading,
-        deleteTask,
-        updateTask,
-      }}
-    >
-      {children}
-    </TasksContext.Provider>
-  );
+      const { error } = await supabase
+         .from("tasks")
+         .update(updateFields)
+         .eq("userId", user.id)
+         .eq("id", id);
+
+      if (error) throw error;
+
+      setTasks(tasks.filter((task) => task.id !== id));
+   };
+
+   return (
+      <TasksContext.Provider
+         value={{
+            tasks,
+            getTasks,
+            createTask,
+            adding,
+            loading,
+            deleteTask,
+            updateTask,
+         }}>
+         {children}
+      </TasksContext.Provider>
+   );
 };
